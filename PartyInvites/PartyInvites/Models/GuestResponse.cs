ae56e26fdba848c8899147841f6ff6a13840d01f
Chris McClure <chris.m.mcclure@gmail.com>
2017-12-02 14:30:05 -0600

    added form validation

--- PartyInvites/PartyInvites/Models/GuestResponse.cs
+++ PartyInvites/PartyInvites/Models/GuestResponse.cs
@@ -2,14 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace PartyInvites.Models
 {
     public class GuestResponse
     {
+        [Required(ErrorMessage = "Please enter your name")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter your email")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage ="Please enter valid email")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please enter your phone")]
         public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Please specify if you will attend")]
         public bool? WillAttend { get; set; }
     }
 }

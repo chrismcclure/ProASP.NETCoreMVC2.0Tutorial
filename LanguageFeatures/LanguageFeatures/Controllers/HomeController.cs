fa5d918b6a1e93622e153571b28d10b1cd949262
Chris McClure <chris.m.mcclure@gmail.com>
2017-12-02 16:32:55 -0600

    Initial setup and demonstrating the null coditional operator with null objects

---
+++ LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using LanguageFeatures.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LanguageFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+       
+        public ViewResult Index()
+        {
+            List<string> results = new List<string>();
+
+            foreach (Product p in Product.GetProducts())
+            {              
+                string name = p?.Name  ?? "<No Name>";
+                decimal? price = p?.Price ?? 0;
+                string relatedName = p?.Related?.Name ?? "<None>";
+                //This causes a big fat null reference exception since one of the products is null in the array ¯\_(ツ)_/¯
+                //bool instock = p.InStock;
+                results.Add(string.Format($"Name: {name}, Price: {price}, Releated {relatedName}"));
+            }
+
+            return View(results);
+          
+        }
+    }
+}
---
+++ LanguageFeatures/LanguageFeatures/Models/Product.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LanguageFeatures.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public string Catagory { get; set; } = "Watersports";
+        public decimal? Price { get; set; }
+        public Product Related { get; set; }
+        public bool InStock { get;}
+
+        public Product(bool stock = true)
+        {
+            InStock = true;
+
+        } 
+
+
+        public static Product[] GetProducts()
+        {
+            Product kayak = new Product
+            {
+                Name = "Kayak",
+                Catagory = "Water Craft",
+                Price = 275M
+            };
+
+            Product lifeJacket = new Product(false)
+            {
+                Name = "Life Jacket",
+                Price = 48.95M
+            };
+
+            kayak.Related = lifeJacket;
+
+            //adding the null to demonstrate how the new null reference check works. Normally this would dumb to do ¯\_(ツ)_/¯
+            return new Product[] 
+            {
+                kayak,
+                lifeJacket,
+                null
+            };
+        }
+    }
+}

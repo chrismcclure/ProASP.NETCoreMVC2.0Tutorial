74e8c017d2b3d6790c10e1c85b2e8330748026c3
Chris McClure <chris.m.mcclure@gmail.com>
2018-01-06 06:22:07 -0600

    Tutorial on extension method and async/await

--- LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
@@ -10,22 +10,43 @@ namespace LanguageFeatures.Controllers
     public class HomeController : Controller
     {
        
-        public ViewResult Index()
+
+        //Practice with async
+        public async Task<ViewResult> Index()
         {
-            List<string> results = new List<string>();
-
-            foreach (Product p in Product.GetProducts())
-            {              
-                string name = p?.Name  ?? "<No Name>";
-                decimal? price = p?.Price ?? 0;
-                string relatedName = p?.Related?.Name ?? "<None>";
-                //This causes a big fat null reference exception since one of the products is null in the array ¯\_(ツ)_/¯
-                //bool instock = p.InStock;
-                results.Add(string.Format($"Name: {name}, Price: {price}, Releated {relatedName}"));
-            }
-
-            return View(results);
-          
+            long? length = await MyAsyncMethods.GetPageLength();
+            return View(new string[] { $"Length: {length}" });
         }
+
+
+
+        //public ViewResult Index()
+        //{
+        //    List<string> results = new List<string>();
+
+        //    foreach (Product p in Product.GetProducts())
+        //    {
+
+        //        ShoppingCart cart = new ShoppingCart { Products = Product.GetProducts() };
+
+        //        //This is the sweet spot, here is the use of extension method. 
+        //        decimal cartTotal = cart.TotalPrices();
+
+
+
+        //        string name = p?.Name  ?? "<No Name>";
+        //        decimal? price = p?.Price ?? 0;
+        //        string relatedName = p?.Related?.Name ?? "<None>";
+        //        //This causes a big fat null reference exception since one of the products is null in the array 
+        //        //I could have just check to see if "p" was null ¯\_(ツ)_/¯ like this 
+        //        //if(p != null)
+        //        //Then do the rest of the stuff
+        //        //bool instock = p.InStock;
+        //        results.Add(string.Format($"Name: {name}, Price: {price}, Releated {relatedName}"));
+        //    }
+
+        //    return View(results);
+          
+        //}
     }
 }
---
+++ LanguageFeatures/LanguageFeatures/Models/MyExtensionMethods.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LanguageFeatures.Models
+{
+    public static class MyExtensionMethods
+    {
+
+        public static decimal TotalPrices(this ShoppingCart cartParm)
+        {
+
+            decimal total = 0;
+            foreach (Product prod in cartParm.Products)
+            {
+                total += prod?.Price ?? 0;
+            }
+            return total;
+        }
+
+    }
+}
